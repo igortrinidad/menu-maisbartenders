a6872bed3111f48c6539853d3a45e4c8640181ea
igortrinidad <igorlucast@hotmail.com>
2017-05-23 10:22:48 -0300

    Added Event model

---
+++ src/models/Event.js
@@ -0,0 +1,91 @@
+export default {
+	event_type: 'Casamento',
+	date: '2017-10-10',
+	name: 'João e Joana',
+	greeting: 'Sejam bem vindos ao nosso casamento',
+	event_photo_url: '',
+	drinks: [
+		{
+			name: 'Trinidad',
+			description: 'Um drink muito top!',
+			items: [
+				{
+					name: 'Morango',
+					category: 'Frutas',
+
+				},
+				{
+					name: 'Gin',
+					category: 'Bebidas',
+
+				},
+				{
+					name: 'Espuma de jabuticaba',
+					category: 'Ingredientes',
+
+				},
+				{
+					name: 'Pimenta do reino',
+					category: 'Ingredientes',
+
+				}
+			],
+			presentation: 'Taça Martini',
+			style: 'No Jeito',
+			photo_url: 'https://maisbartenders.com.br/img/portfolio/Mais_Bartenders_TRINIDAD.JPG'
+		},
+		{
+			name: 'Moscow Mule',
+			description: 'O melhor Moscow do Brasil!',
+			items: [
+				{
+					name: 'Refrigerante artesanal de gengibre',
+					category: 'Ingredientes',
+
+				},
+				{
+					name: 'Vodka',
+					category: 'Bebidas',
+
+				},
+				{
+					name: 'Espuma de gengibre',
+					category: 'Ingredientes',
+
+				}
+			],
+			presentation: 'Caneca esmaltada',
+			style: 'No Jeito',
+			photo_url: 'https://instagram.fplu4-1.fna.fbcdn.net/t51.2885-15/s750x750/sh0.08/e35/17595854_430075987339773_5640327486068752384_n.jpg'
+		},
+		{
+			name: 'Di Souza',
+			description: 'Um drink excelente para começar o dia!',
+			items: [
+				{
+					name: 'Abacaxi',
+					category: 'Frutas',
+
+				},
+				{
+					name: 'Martini Dry',
+					category: 'Bebidas',
+
+				},
+				{
+					name: 'Licor de gengibre',
+					category: 'Bebidas',
+
+				},
+				{
+					name: 'Hortelã',
+					category: 'Hortaliças',
+
+				}
+			],
+			presentation: 'Taça Martini',
+			style: 'Leve',
+			photo_url: 'https://maisbartenders.com.br/img/portfolio/di_souza2.JPG'
+		}
+	]
+}
