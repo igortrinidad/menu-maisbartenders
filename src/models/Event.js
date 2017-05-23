09868a894143ccd9268a8dd7559f5c930ac0c0e0
igortrinidad <igorlucast@hotmail.com>
2017-05-23 10:39:18 -0300

    Minor change on model

--- src/models/Event.js
+++ src/models/Event.js
@@ -2,12 +2,14 @@ export default {
 	event_type: 'Casamento',
 	date: '2017-10-10',
 	name: 'João e Joana',
+	url: 'joana-joao',
 	greeting: 'Sejam bem vindos ao nosso casamento',
 	event_photo_url: '',
 	drinks: [
 		{
 			name: 'Trinidad',
 			description: 'Um drink muito top!',
+			priority: 5,
 			items: [
 				{
 					name: 'Morango',
@@ -37,6 +39,7 @@ export default {
 		{
 			name: 'Moscow Mule',
 			description: 'O melhor Moscow do Brasil!',
+			priority: 5,
 			items: [
 				{
 					name: 'Refrigerante artesanal de gengibre',
@@ -61,6 +64,7 @@ export default {
 		{
 			name: 'Di Souza',
 			description: 'Um drink excelente para começar o dia!',
+			priority: 5,
 			items: [
 				{
 					name: 'Abacaxi',
@@ -86,6 +90,26 @@ export default {
 			presentation: 'Taça Martini',
 			style: 'Leve',
 			photo_url: 'https://maisbartenders.com.br/img/portfolio/di_souza2.JPG'
-		}
+		},
+		{
+			name: 'Caipi fruta de Kiwi',
+			description: 'Refrescante!',
+			priority: 2,
+			items: [
+				{
+					name: 'Kiwi',
+					category: 'Frutas',
+
+				},
+				{
+					name: 'Vodka',
+					category: 'Bebidas',
+
+				}
+			],
+			presentation: 'On the rocks',
+			style: 'No jeito',
+			photo_url: ''
+		},
 	]
 }
