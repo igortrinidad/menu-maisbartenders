fcb289217ede111ee518230dde756def1bf698a5
igortrinidad <igorlucast@hotmail.com>
2017-05-26 16:48:04 -0300

    Fix config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
-    API_URL: '"https://maisbartenders.dev/api"',
+    API_URL: '"https://maisbartenders.com.br/api"',
     FACEBOOK_CLIENT_ID: '"FACEBOOK_CLIENT_ID"',
     GOOGLE_CLIENT_ID: '"GOOGLE_CLIENT_ID"',
 }
