3304556d633ece2f1a171a08c5cf1fc741276f97
Andre <andrebf4gmail.com>
2017-05-26 14:57:33 -0300

    cache files

--- static/build/js/serviceWorker.js
+++ static/build/js/serviceWorker.js
@@ -1,12 +1,34 @@
+let files = [
+    // routes
+    '/',
+
+    // css files
+    '/static/build/css/vendor.css',
+    '/static/build/css/agency.css',
+
+    // static images
+    '/static/assets/header-bg.jpg',
+
+    // js files
+    '/static/build/js/vendor.js',
+    '/app.js',
+    '/static/build/js/services/register.js'
+]
+
+// install application
+self.addEventListener('install', function() {
+    console.log('installing')
+    caches.open('ceep-files').then(cache => {
+        cache.addAll(files)
+    })
+})
+
+// fetch application files
 self.addEventListener('fetch', function(event) {
     let order = event.request
-
-    let promisseResponse = chaces.open('ceep-images').then(cache => {
-        return cache.match(order)
-    }).then(response => {
+    console.log('fetching')
+    let promisseResponse = chace.match(order).then(response => {
         return response ? response : fetch(order)
     })
-
     event.respondWith(promisseResponse)
-
 })
