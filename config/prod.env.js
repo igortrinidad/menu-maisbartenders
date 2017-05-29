603e2db27addff2ea3d5c8844bf90d18181a6b79
Matheus Lima <contato@matheuslima.com.br>
2017-05-29 17:42:55 -0400

    Added social client ids on production

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     API_URL: '"https://maisbartenders.com.br/api"',
-    FACEBOOK_CLIENT_ID: '"FACEBOOK_CLIENT_ID"',
-    GOOGLE_CLIENT_ID: '"GOOGLE_CLIENT_ID"',
+    FACEBOOK_CLIENT_ID: '"262783620860879"',
+    GOOGLE_CLIENT_ID: '"78576259068-946rmqo5lr86v1hekodh2v6qsjpm9p23.apps.googleusercontent.com"',
 }
