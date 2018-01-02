e074a071035e7554ded106614be995ee5aa93659
Andre Brandao <andrebf4@gmail.com>
2018-01-02 16:23:02 -0200

    build generated to apply styles

--- www/static/js/7.9e86e82831b481f36858.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([7],{233:function(e,n,t){var r=t(1)(t(542),t(564),null,null);e.exports=r.exports},542:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={name:"dashboard-main"}},564:function(e,n){e.exports={render:function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},staticRenderFns:[]}}});
-//# sourceMappingURL=7.9e86e82831b481f36858.js.map
