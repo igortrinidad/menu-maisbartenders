e87c5bb412a291af944163ba29ead4d2a76ebe39
Andre Brandao <andrebf4@gmail.com>
2018-01-04 12:15:30 -0200

    new style to simulator

--- src/modules/landing/pages/simulator.vue
+++ src/modules/landing/pages/simulator.vue
@@ -1,128 +1,122 @@
 <template lang="html">
-    <section>
+    <div class="first-container">
 
         <main-header :title="'Simulador'" />
 
+        <!-- Icon SVG + Title -->
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Simulador</h2>
-                    <h3 class="section-subheading text-muted">Calcule a quantidade de bebidas para seu evento.</h3>
-                </div>
+            <div class="svg-container">
+                <svg viewBox="0 0 60 60">
+                    <defs>
+                        <linearGradient id="linear" x1="0%" y1="0%" x2="100%" y2="0%">
+                            <stop offset="0%"   stop-color="#FB923B"/>
+                            <stop offset="100%" stop-color="#F66439"/>
+                        </linearGradient>
+                    </defs>
+
+                    <path class="non-fill xs fix animated" stroke="url(#linear)"
+                       d="m 27.5,54.5 c -14.888,0 -27,-12.112 -27,-27 0,-14.888 12.112,-27 27,-27 14.888,0 27,12.112 27,27 0,14.888 -12.112,27 -27,27 z m 2,-5 h -4 c -1.103,0 -2,-0.897 -2,-2 v -2 h -1 C 16.893,45.5 12.194,41.302 11.569,35.735 11.435,34.546 12.368,33.5 13.563,33.5 h 4.042 c 0.952,0 1.757,0.667 1.959,1.622 0.292,1.378 1.527,2.378 2.936,2.378 h 9.822 c 1.626,0 3.016,-1.185 3.164,-2.697 C 35.663,32.98 34.221,31.5 32.5,31.5 H 22.853 C 16.886,31.5 11.91,26.998 11.525,21.251 11.09,14.75 16.283,9.5 22.5,9.5 h 1 v -2 c 0,-1.103 0.897,-2 2,-2 h 4 c 1.103,0 2,0.897 2,2 v 2 h 1 c 5.607,0 10.306,4.198 10.931,9.765 0.134,1.189 -0.799,2.235 -1.994,2.235 h -4.042 c -0.952,0 -1.757,-0.667 -1.959,-1.622 C 35.144,18.5 33.909,17.5 32.5,17.5 h -9.822 c -1.626,0 -3.016,1.185 -3.164,2.697 -0.177,1.823 1.265,3.303 2.986,3.303 h 9.647 c 5.967,0 10.943,4.502 11.328,10.249 C 43.91,40.25 38.717,45.5 32.5,45.5 h -1 v 2 c 0,1.103 -0.897,2 -2,2 z"
+                   />
+                </svg>
             </div>
+            <h4 class="title-section">Calcule a quantidade de bebidas para seu evento.</h4>
         </div>
 
-        <div class="container">
-            <div class="row">
+        <!-- Simulador -->
+        <div class="container-colored m-t-10">
+            <div class="container text-center">
 
                 <!-- Guests -->
-                <div class="col-sm-12 text-center">
-                    <div class="form-group">
-                        <h4>Número de convidados</h4>
-                        <input type="number" class="form-control" v-model="guests">
+                <div class="card">
+                    <div class="card-body card-padding">
+                        <h5 class="card-title" for="guests">Número de convidados</h5>
+                        <input id="guests" type="number" class="form-control" v-model="guests">
                         <p class="help-block text-danger"></p>
                     </div>
                 </div>
-                <!-- / Guests -->
-
-                <div class="col-sm-12">
-                    <hr>
-                </div>
 
                 <!-- Event Type -->
-                <div class="col-sm-12 text-center">
-                     <h4>Tipo do evento?</h4>
-                     <button
-                        v-for="type in event_types"
-                        class="btn inline m-r-5 m-t-5"
-                        @click="addType($event, type)"
-                        :class="{'btn-xl':typeSelected.name == type.name}"
-                    >{{type.name}}
-                     </button>
-                </div>
-                <!-- / Event Type -->
-
-                <div class="col-sm-12">
-                    <hr>
+                <div class="card">
+                    <div class="card-body card-padding">
+                        <h5 class="card-title" for="">Qual o tipo do evento ?</h5>
+                        <button
+                            v-for="type in event_types"
+                            class="btn btn-xs btn-mb-primary"
+                            style="margin: 2px;"
+                            @click="addType($event, type)"
+                            :class="{'outline':typeSelected.name !== type.name}"
+                        >
+                            {{ type.name }}
+                        </button>
+                    </div>
                 </div>
 
                 <!-- Drinks -->
-                <div class="col-sm-12 text-center">
-                    <h4>Bebidas que vou servir?</h4>
-                    <button
-                        v-for="bev in beverages"
-                        class="btn inline m-r-5 m-t-5"
-                        @click="addBeverages($event, bev.id)"
-                        :class="{'btn-xl':bevSelected.indexOf(bev.id) > -1}"
-                    >{{bev.name}}
-                    </button>
-                </div>
-                <!-- / Drinks -->
+                <div class="card">
+                    <div class="card-body card-padding">
+                        <h5 class="card-title">Bebidas que vou servir?</h5>
 
-                <div class="col-sm-12">
-                    <hr>
+                        <button
+                            v-for="bev in beverages"
+                            class="btn btn-xs btn-mb-primary"
+                            style="margin: 2px;"
+                            @click="addBeverages($event, bev.id)"
+                            :class="{'outline':bevSelected.indexOf(bev.id) < 0}"
+                        >{{bev.name}}
+                        </button>
+                    </div>
                 </div>
 
                 <!-- Bar -->
-                <div class="col-sm-12 text-center">
-                    <h4>Vai ter bar de drinks?</h4>
-                    <button
-                        class="btn inline"
-                        @click="toggle(has_bar, true)"
-                        :class="{'btn-xl':has_bar.selected && sum > 0.4 && is_clicked_bar}"
+                <div class="card">
+                    <div class="card-body card-padding">
+                        <h5 class="card-title">Vai ter bar de drinks?</h5>
+
+                        <button
+                            class="btn btn-mb-primary outline"
+                            @click="toggle(has_bar, true)"
+                            :class="{'reset-outline':has_bar.selected && sum > 0.4 && is_clicked_bar}"
+                            >Sim
+                        </button>
+                        <button
+                            class="btn btn-mb-primary outline"
+                            @click="toggle(has_bar, false)"
+                            :class="{'reset-outline':!has_bar.selected && sum > 0.4 && is_clicked_bar}"
+                            data-toggle="modal"
+                            data-target="#modalReactionCry"
+                        >Não
+                        </button>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-body card-padding">
+                        <h5 class="card-title">O bar será da Mais Bartenders?</h5>
+                        <button
+                            class="btn btn-mb-primary outline"
+                            :class="{'reset-outline':has_mb.selected && sum > 0.4 && is_clicked_mb}"
+                            @click="toggle(has_mb, true)"
+                            data-toggle="modal"
+                            data-target="#modalReaction"
                         >Sim
-                    </button>
-                    <button
-                        class="btn  inline"
-                        @click="toggle(has_bar, false)"
-                        :class="{'btn-xl':!has_bar.selected && sum > 0.4 && is_clicked_bar}"
-                        data-toggle="modal"
-                        data-target="#modalReactionCry"
-                    >Não
-                    </button>
-                </div>
-                <!-- Bar -->
-
-                <div class="col-sm-12">
-                    <hr>
-                </div>
-
-                <!-- Is MaisBartenders ? -->
-                <div class="col-sm-12 text-center">
-                    <h4>O bar será da Mais Bartenders?</h4>
-                    <button
-                        class="btn  inline"
-                        :class="{'btn-xl':has_mb.selected && sum > 0.4 && is_clicked_mb}"
-                        @click="toggle(has_mb, true)"
-                        data-toggle="modal"
-                        data-target="#modalReaction"
-                    >Sim
-                    </button>
-                    <button
-                        class="btn inline"
-                        :class="{'btn-xl':!has_mb.selected && sum > 0.4 && is_clicked_mb}"
-                        @click="toggle(has_mb, false)"
-                        data-toggle="modal"
-                        data-target="#modalReactionCry"
-                    >Não</button>
-                </div>
-                <!-- / Is MaisBartenders ? -->
-
-                <div class="col-sm-12">
-                    <hr>
+                        </button>
+                        <button
+                            class="btn btn-mb-primary outline"
+                            :class="{'reset-outline':!has_mb.selected && sum > 0.4 && is_clicked_mb}"
+                            @click="toggle(has_mb, false)"
+                            data-toggle="modal"
+                            data-target="#modalReactionCry"
+                        >Não</button>
+                    </div>
                 </div>
 
                 <!-- Calculate -->
-                <div class="col-sm-12 text-center">
-                    <button
-                        class="btn inline btn-xl"
-                        @click="openResult()"
-                        style="background-color:#FA923C;margin: 20px 0 0 0;border: 0px"
-                    >Calcular</button>
-                </div>
-                <!-- / Calculate -->
+                <button class="btn btn-block btn-mb-primary-reverse"@click="openResult()">Calcular</button>
+
             </div>
         </div>
+
+
 
         <!-- Modal Party Reaction -->
         <div class="modal fade" id="modalReaction" tabindex="-1" data-backdrop="false"  aria-labelledby="myModalLabel">
@@ -199,7 +193,7 @@
             </div>
         </div>
         <!-- / Modal Result -->
-    </section>
+    </div>
 </template>
 
 <script>
@@ -405,15 +399,9 @@ export default {
 
 <style scoped>
 
-    .btn-xl { padding: 10px 20px; }
-
-    .btn:hover, .btn:focus, .btn.focus { color: #2c3e50 !important; }
-    .btn-xl:hover,
-    .btn-xl:focus,
-    .btn-xl:active,
-    .btn-xl.active,
-    .open .dropdown-toggle.btn-xl
-    { color: #2c3e50 !important; background-color: #FED13B; border-color: #FED13B; }
+    .btn {
+        border-width: 2px;
+    }
 
     thead tr th {
         vertical-align: middle;
