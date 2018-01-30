27eebfcc063afab1424c586a16388ca2397b21dc
Igor Trindade <igorlucast@hotmail.com>
2018-01-30 18:28:14 -0200

    WTF

---
+++ www/static/js/8.5eae518c3dff47d331ca.js
@@ -0,0 +1,2 @@
+webpackJsonp([8],{225:function(e,t,n){var r=n(1)(n(536),n(560),null,null);e.exports=r.exports},536:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default={name:"dashboard-home",data:function(){return{}},methods:{}}},560:function(e,t){e.exports={render:function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},staticRenderFns:[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"container"},[n("h1",[e._v("Welcome to the dashboard")])])}]}}});
+//# sourceMappingURL=8.5eae518c3dff47d331ca.js.map
